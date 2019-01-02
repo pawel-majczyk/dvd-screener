accba663e05ac0ebd557de7c0079672f79de69a8
Paweł Majczyk <pawel.majczyk@gmail.com>
2019-01-02 19:23:38 +0100

    default scaffold fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,10 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
     '@vue/airbnb',
   ],
   rules: {
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: 'assets',
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined,
+};
